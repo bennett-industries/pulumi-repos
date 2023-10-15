f9ab2771872005aafd4ff76fb6feacdb0750499e
Brandon Bennett <brandonbennett@gmail.com>
2023-10-15 11:19:20 -0400

    First attempt at file upload

--- bennett-industries/repos.go
+++ bennett-industries/repos.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"os"
 
 	"github.com/pulumi/pulumi-github/sdk/v5/go/github"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func repoSetup(ctx *pulumi.Context, repo Repo) error {
-  // Create Repos
+	// Create Repos
 	repository, err := github.NewRepository(ctx, repo.Name, &github.RepositoryArgs{
 		Name:                pulumi.String(repo.Name),
 		Description:         pulumi.String(repo.Desc),
@@ -19,26 +20,59 @@ func repoSetup(ctx *pulumi.Context, repo Repo) error {
 		return err
 	}
 
-  // Create Environments for Repos
-	for _, e := range repo.Environments {
-    name := fmt.Sprintf("%v-%v", repo.Name , e)
-  	_, err = github.NewRepositoryEnvironment(ctx, name, &github.RepositoryEnvironmentArgs{
-  		Environment: pulumi.String(e.Name),
-  		Repository:  repository.Name,
-  		DeploymentBranchPolicy: &github.RepositoryEnvironmentDeploymentBranchPolicyArgs{
-  			ProtectedBranches:    pulumi.Bool(true),
-  			CustomBranchPolicies: pulumi.Bool(false),
-  		},
-  	})
-  	if err != nil {
-  		return err
-  	}
-  }
 
-  // Create Actions Environment Varaibles
-  // TODO: This isn't working atm
+	// Add global files to Repos
+
+	gfiles, err := os.ReadDir("./global/")
+	if err != nil {
+		return err
+	}
+
+	for _, file := range gfiles {
+		fileNameForState := fmt.Sprintf("global-file-%v-%v", repo.Name, file)
+
+		fileContent, err := os.ReadFile(file.Name())
+		if err != nil {
+			return err
+		}
+
+		_, err = github.NewRepositoryFile(ctx, fileNameForState, &github.RepositoryFileArgs{
+			Repository:        pulumi.String(repo.Name),
+			Branch:            pulumi.String("main"),
+			File:              pulumi.String(file.Name()),
+			Content:           pulumi.String(fileContent),
+			CommitMessage:     pulumi.String("Global File Added by Pulumi"),
+			CommitAuthor:      pulumi.String("Pulumi"),
+			CommitEmail:       pulumi.String("pulumi@example.com"),
+			OverwriteOnCreate: pulumi.Bool(true),
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	// Create Environments for Repos
+	for _, e := range repo.Environments {
+		name := fmt.Sprintf("%v-%v", repo.Name, e)
+		_, err = github.NewRepositoryEnvironment(ctx, name, &github.RepositoryEnvironmentArgs{
+			Environment: pulumi.String(e.Name),
+			Repository:  repository.Name,
+			DeploymentBranchPolicy: &github.RepositoryEnvironmentDeploymentBranchPolicyArgs{
+				ProtectedBranches:    pulumi.Bool(true),
+				CustomBranchPolicies: pulumi.Bool(false),
+			},
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	// Create Actions Environment Varaibles
+	// TODO: This isn't working atm
+	// TODO: Add a switch to check for feature avaialability
+
 	//for _, ev := range repo.EnvVars {
-  // // TODO: add option for adding a var to all envs when specified
+	// // TODO: add option for adding a var to all envs when specified
 	//  _, err := github.NewActionsEnvironmentVariable(ctx, ev.Name, &github.ActionsEnvironmentVariableArgs{
 	//  	Environment:  pulumi.String(ev.Environment),
 	//  	Value:        pulumi.String(ev.Value),
